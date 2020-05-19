89eef84cc64bd25552df42394914391bcf33340f
Bpedley <v3rtex12@gmail.com>
2020-05-19 17:44:53 +0300

    Removed repeated code in actions

--- src/redux/actions/Posts/index.js
+++ src/redux/actions/Posts/index.js
@@ -8,50 +8,40 @@ import {
 } from "./types";
 import { API } from "../../../api";
 
-export const getPosts = () => async dispatch => {
+const postsFlow = async (dispatch, fetchType, apiMethod, fetchTypeAC, data) => {
   dispatch({ type: FETCHING_TOGGLE });
-  dispatch({ type: FETCH_TYPE, payload: "getPosts" });
-  const response = await API.getPosts();
+  dispatch({ type: FETCH_TYPE, payload: fetchType });
 
-  if (response.statusText === "OK") {
-    dispatch({ type: GET_POSTS, payload: response.data });
+  const response = await apiMethod(data);
+
+  if (response.status >= 200 && response.status < 300) {
+    dispatch({ type: fetchTypeAC, payload: response.data });
     dispatch({ type: FETCHING_TOGGLE });
   } else {
     dispatch({
       type: ERROR,
       payload: {
         errorMessage: response,
-        errorType: "getPosts"
+        errorType: fetchType
       }
     });
     dispatch({ type: FETCHING_TOGGLE });
   }
+};
+
+export const getPosts = () => async dispatch => {
+  const fetchType = "getPosts";
+  postsFlow(dispatch, fetchType, API.getPosts.bind(API), GET_POSTS, null);
 };
 
 export const createPost = (e, postData) => async dispatch => {
   e.preventDefault();
-  dispatch({ type: FETCHING_TOGGLE });
-  dispatch({ type: FETCH_TYPE, payload: "createPost" });
-  const response = await API.createPost(postData);
-
-  if (response.statusText === "Created") {
-    dispatch({ type: NEW_POST, payload: response.data });
-    dispatch({ type: FETCHING_TOGGLE });
-  } else {
-    dispatch({
-      type: ERROR,
-      payload: {
-        errorMessage: response,
-        errorType: "createPost"
-      }
-    });
-    dispatch({ type: FETCHING_TOGGLE });
-  }
+  const fetchType = "createPost";
+  postsFlow(dispatch, fetchType, API.createPost.bind(API), NEW_POST, postData);
 };
 
 export const setInputFields = e => {
   const { name, value } = e.target;
-
   return {
     type: SET_INPUT_FIELDS,
     payload: { name, value }
